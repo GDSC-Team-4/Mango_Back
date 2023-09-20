ffcadbe2abb91a072a7810064ca419643ee66c80
Seo Eun <seo606101@naver.com>
2023-09-20 20:55:19 +0900

    init

---
+++ src/main/java/com/example/mangoplace/MangoplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangoplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mangoplace/MangoplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangoplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
