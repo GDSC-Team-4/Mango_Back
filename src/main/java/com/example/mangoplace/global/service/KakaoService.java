8d5eb53d5103737ce6eff94ec9a8f886fa8e253b
Su-daa <subin.kk019@gmail.com>
2023-12-15 23:44:52 +0900

    Feat: 상세 페이지 조회

--- src/main/java/com/example/mangoplace/global/service/KakaoService.java
+++ src/main/java/com/example/mangoplace/global/service/KakaoService.java
@@ -1,9 +1,12 @@
 package com.example.mangoplace.global.service;
 
+import com.example.mangoplace.domain.review.entity.Review;
+import com.example.mangoplace.domain.scrap.repository.ScrapRepository;
 import com.example.mangoplace.domain.shop.entity.Shop;
 import com.example.mangoplace.domain.shop.repository.ShopRepository;
 import com.example.mangoplace.global.dto.KakaoPlace;
 import com.example.mangoplace.global.dto.Place;
+import com.example.mangoplace.global.dto.PlaceDetailResponseDto;
 import com.example.mangoplace.global.dto.PlaceResponseDto;
 import com.example.mangoplace.global.repository.KakaoRepository;
 import lombok.AllArgsConstructor;
@@ -21,6 +24,7 @@ public class KakaoService {
 
     private final KakaoRepository kakaoRepository;
     private final ShopRepository shopRepository;
+    private final ScrapRepository scrapRepository;
 
     /**
      * kakaoplace -> place 작업을 여기서
@@ -71,4 +75,16 @@ public class KakaoService {
         return placeResponseDtoList;
     }
 
+    public PlaceDetailResponseDto placeDetail(String shopId) {
+        Shop shop = shopRepository.findByRestaurantId(shopId).orElseThrow(IllegalArgumentException::new);
+        List<Review> reviews = shop.getReviews();
+
+        String restaurantId = shop.getRestaurantId(); //shopId==restaurantId
+        Long scrapCounts = scrapRepository.countByShop_RestaurantId(restaurantId);
+
+        return PlaceDetailResponseDto.builder()
+                .reviews(reviews)
+                .scrapCount(scrapCounts)
+                .build();
+    }
 }
