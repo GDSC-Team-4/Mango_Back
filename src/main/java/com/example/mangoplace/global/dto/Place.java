4c10367f9d56ae3121f4cf7aee01cdb30e23d08d
Su-daa <subin.kk019@gmail.com>
2023-12-18 16:43:36 +0900

    Hotfix: 식당 관련 응답 수정

--- src/main/java/com/example/mangoplace/global/dto/Place.java
+++ src/main/java/com/example/mangoplace/global/dto/Place.java
@@ -1,8 +1,10 @@
 package com.example.mangoplace.global.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.parameters.P;
 
 /**
  * 프론트한테 줄 객체
@@ -12,13 +14,29 @@ import lombok.Getter;
 public class Place {
     private String id;
     private String placeName;
+    private String categoryName;
+    private String categoryGroupCode;
+    private String categoryGroupName;
+    private String phone;
+    private String addressName;
     private String roadAddressName;
+    private String x;
+    private String y;
     private String placeUrl;
 
     public static Place from(KakaoPlace kakaoPlace) {
-        return new Place(kakaoPlace.getId(),
+        return new Place(
+                kakaoPlace.getId(),
                 kakaoPlace.getPlaceName(),
+                kakaoPlace.getCategoryName(),
+                kakaoPlace.getCategoryGroupCode(),
+                kakaoPlace.getCategoryGroupName(),
+                kakaoPlace.getPhone(),
+                kakaoPlace.getAddressName(),
                 kakaoPlace.getRoadAddressName(),
-                kakaoPlace.getPlaceUrl());
+                kakaoPlace.getX(),
+                kakaoPlace.getY(),
+                kakaoPlace.getPlaceUrl()
+        );
     }
 }
