2d4e2c05e5bc63921928ff296b61025693ee85ff
Su-daa <subin.kk019@gmail.com>
2023-12-18 13:00:44 +0900

    Hotfix: 상세페이지 get 401 오류 해결

--- src/main/java/com/example/mangoplace/domain/auth/security/WebSecurityConfig.java
+++ src/main/java/com/example/mangoplace/domain/auth/security/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
                                // .requestMatchers("/**").permitAll() //다음 엔드포인트를 인증없이 허용
                                 .requestMatchers("/search").permitAll()
                                 .requestMatchers("/main").permitAll()
-                                .requestMatchers("/shops/**").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/shops/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/review/**").permitAll()
                                 .requestMatchers("/review/**").authenticated()
                                 .anyRequest().authenticated() //그 외 모든 요청 인증된 사용자에게만 허용
